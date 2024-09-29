9099f5a2f937715bc245f03253a6f1b40d0dccef
CamiloMontoya804 <camontoya804@gmail.com>
2024-09-29 00:00:38 -0500

    [F] chore: add timezone to moment

--- frontend/src/utils/utils.ts
+++ frontend/src/utils/utils.ts
@@ -1,9 +1,9 @@
 import * as moment from "moment-timezone";
 
 export const formatDate = (dateString: string) => {
-  return moment.tz(dateString, '84').format('DD/MM/YYYY');
+  return moment.tz(dateString, "America/Bogota").format('DD/MM/YYYY');
 };
 
 export const formatTime = (dateString: string) => {
-  return moment.tz(dateString, '84').format('HH:mm:ss');
+  return moment.tz(dateString, "America/Bogota").format('HH:mm:ss');
 };
